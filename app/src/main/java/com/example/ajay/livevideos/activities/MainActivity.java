3fa3ea4ba7fb07b662c08c73781913f39d29b4d3
ajay <ajay.maheshwari@ranosys.com>
2017-11-10 12:30:08 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/ajay/livevideos/activities/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ajay.livevideos.activities;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.FrameLayout;
+
+import com.example.ajay.livevideos.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}

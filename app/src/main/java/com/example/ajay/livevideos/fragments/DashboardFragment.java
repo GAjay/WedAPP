1f460bcf56c69491ce2f6955c39b97b3e6664cc6
ajay <ajay.maheshwari@ranosys.com>
2017-11-28 12:02:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ajay/livevideos/fragments/DashboardFragment.java
@@ -0,0 +1,11 @@
+package com.example.ajay.livevideos.fragments;
+
+/**
+ * A class for TODO add Class defination.
+ *
+ * @author : Ajay Kumar Maheshwari.
+ *         Copyright (c) 2017 livetechmonk. All right reserved.
+ */
+
+public class DashboardFragment {
+}
